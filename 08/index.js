709b107458aa50824d664c709df4eb6f936d5b4e
Sthefany Silva <sthefany0011@gmail.com>
2023-07-06 19:09:26 -0300

    Resolução do exercicio 08

--- 08/index.js
+++ 08/index.js
@@ -3,3 +3,15 @@ const possuiPatologia = false;
 const altura = 180;
 const ehEstudante = false;
 
+if(idade>12 ||idade<65||possuiPatologia==true||altura<150){
+
+    if(ehEstudante==true||idade<18){
+        console.log("10 reais")
+    }
+    else{
+        console.log("20 reais")
+    }
+}
+else{
+    console.log("ACESSO NEGADO")
+}
