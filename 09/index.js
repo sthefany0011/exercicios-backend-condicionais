5ec39edb35d3673edf0eec4549be2b60f689111e
Sthefany Silva <sthefany0011@gmail.com>
2023-07-06 19:44:59 -0300

    Resolução do exercicio 09

--- 09/index.js
+++ 09/index.js
@@ -1,1 +1,18 @@
 const nota = 8.5;
+
+if(nota >= 9.0 && nota <=10.0){
+    console.log("A")
+}
+else if(nota >= 8.0 && nota <= 8.9){
+    console.log("B")
+}
+else if(nota >= 6.0 && nota <= 7.9){
+    console.log("C")
+}
+else if(nota >= 4.0 && nota <= 5.9){
+    console.log("D")
+}
+else {
+    console.log("E")
+}
+
